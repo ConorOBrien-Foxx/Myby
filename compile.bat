9f5cc1fb091aa858397a087a8a1505470c3d7d5a
ConorOBrien-Foxx <conorobrien4god@gmail.com>
2023-01-03 13:36:50 -0500

    figure out how to use wildcards in Batch :/

--- compile.bat
+++ compile.bat
@@ -7,10 +7,14 @@ IF "%1"=="clean" GOTO :CLEAN
 :COMPILE
     SET FLAGS=-g -w
     REM TODO: surely there's a way to emulate src/*.d
-    SET FILES=src/main.d src/integer.d src/nibble.d src/literate.d src/interpreter.d
-    SET FILES=%FILES% src/instructions.d src/string.d src/debugger.d src/prime.d
-    SET FILES=%FILES% src/format.d src/speech.d src/manip.d src/memo.d src/condense.d
-    SET FILES=%FILES% src/token.d src/json.d
+    REM SET FILES=src/main.d src/integer.d src/nibble.d src/literate.d src/interpreter.d
+    REM SET FILES=%FILES% src/instructions.d src/string.d src/debugger.d src/prime.d
+    REM SET FILES=%FILES% src/format.d src/speech.d src/manip.d src/memo.d src/condense.d
+    REM SET FILES=%FILES% src/token.d src/json.d
+    REM SET FILES=%FILES% src/verbs.d src/adjectives.d src/conjunctions.d
+    SETLOCAL EnableDelayedExpansion
+    SET FILES=src\main.d
+    FOR %%A IN (src\*.d) DO IF "%%~nxA" NEQ "main.d" SET FILES=!FILES! %%A
     dmd %FLAGS% %FILES% -of=myby.exe
     SET Error=%ERRORLEVEL%
     GOTO :End
